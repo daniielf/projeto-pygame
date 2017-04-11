3d40663143e8d42a9b765de50fedab98c12a5d68
daniielf <daniielf.13@gmail.com>
2017-04-10 21:02:15 -0300

    collision working better

--- source/ti/game.py
+++ source/ti/game.py
@@ -34,20 +34,23 @@ class Player (GameObject, pygame.font.Font):
         self.image = pygame.image.load('../media/bob_cima.png')
         self.cash = 0
         self.c_card = 0
+        self.time = 180
+        
         self.rect = pygame.Rect (500,300, 50,50)
         pygame.font.Font.__init__(self, font, font_size)
         
         self.walls = walls
         
-        self.cashLabel = self.render(str(self.cash), 1, (0,0,0))
-        self.c_cardLabel = self.render(str(self.c_card),1,(0,0,0))
+        self.timeLabel = self.render("Tempo:" + str(self.time), 1, (0,0,0))
+        self.cashLabel = self.render("Dinheiro:" + str(self.cash), 1, (0,0,0))
+        self.c_cardLabel = self.render("Cartoes:" + str(self.c_card),1,(0,0,0))
         
     def moveUp(self):
         block_hit_list = pygame.sprite.spritecollide(self, self.walls, False)
         self.image = pygame.image.load('../media/bob_cima.png')
         self.rect.top -= self.acceleration
         for wall in block_hit_list:
-            if self.rect.top < wall.rect.bottom:
+            if self.rect.top - 10 < wall.rect.bottom:          
                 self.rect.top = wall.rect.bottom
             else:
                 self.rect.top -= self.acceleration
@@ -59,7 +62,7 @@ class Player (GameObject, pygame.font.Font):
         self.rect.bottom += self.acceleration
         #print len(block_hit_list)
         for wall in block_hit_list:
-            if self.rect.bottom > wall.rect.top: 
+            if self.rect.bottom + 10> wall.rect.top:          
                 self.rect.bottom = wall.rect.top
             else:    
                 self.rect.bottom += self.acceleration
@@ -69,7 +72,7 @@ class Player (GameObject, pygame.font.Font):
         self.image = pygame.image.load('../media/bob_esquerda.png')
         self.rect.left -= self.acceleration
         for wall in block_hit_list:
-            if self.rect.left < wall.rect.right:
+            if self.rect.left - 10 < wall.rect.right:            
                 self.rect.left = wall.rect.right              
             else:
                 self.rect.left -= self.acceleration
@@ -79,15 +82,16 @@ class Player (GameObject, pygame.font.Font):
         self.image = pygame.image.load('../media/bob_direita.png')
         self.rect.right += self.acceleration
         for wall in block_hit_list:
-            if self.rect.right > wall.rect.left:
+            if self.rect.right + 10> wall.rect.left:          
                 self.rect.right = wall.rect.left
             else:
                 self.rect.right += self.acceleration
         
         
     def updateValues(self):
-        self.cashLabel = self.render(str(self.cash),1,(0,0,0))
-        self.c_cardLabel = self.render(str(self.c_card),1,(0,0,0))
+        self.timeLabel = self.render("Tempo:" + str(self.time), 1, (0,0,0))
+        self.cashLabel = self.render("Dinheiro:" + str(self.cash), 1, (0,0,0))
+        self.c_cardLabel = self.render("Cartoes:" + str(self.c_card), 1,(0,0,0))
         
 
 class Wall (GameObject):
@@ -110,9 +114,7 @@ class Game ():
         self.bg_color = (255,255,255)
         #self.image
         self.player = Player("image", 300 , 500, 50 , 50, 0)
-
-        
-        
+     
     
     def run(self):
         bob = self.player
@@ -153,24 +155,30 @@ class Game ():
         for wall in obstacles:
             pygame.draw.rect(self.screen, (0,0,0), wall.rect, 0)
         
-        
+        time_decrement = pygame.USEREVENT+1
+        T = 1000 # second
+        pygame.time.set_timer(time_decrement, T)
         
         gameRunning = True
         while (gameRunning):
             clock.tick(60)
+            
+            
             for event in pygame.event.get():
+                if (event.type == time_decrement):
+                    self.player.time -= 1
                 if (event.type == pygame.KEYDOWN):
-                    
+                    pygame.event.set_blocked(pygame.KEYDOWN)
                     if (event.key == pygame.K_ESCAPE):
                         gameRunning = False
                     elif (event.key == pygame.K_z): 
                         bob.cash += 1 
-                        bob.updateValues()
                         bob.rect.x = 480
                         bob.rect.y = 280
                     elif (event.key == pygame.K_UP):
                         bob.acceleration = 5
                         bob.direction = "up"
+                        
                        
                     elif (event.key == pygame.K_DOWN):
                         bob.acceleration = 5
@@ -183,10 +191,12 @@ class Game ():
                     elif (event.key == pygame.K_RIGHT):
                         bob.acceleration = 5    
                         bob.direction = "right"
+
                 if (event.type == pygame.KEYUP):
                     bob.acceleration = 0
+                    pygame.event.set_allowed(pygame.KEYDOWN)
             
-            
+            bob.updateValues()
             if (bob.direction == "up"):
                 bob.moveUp()
             elif (bob.direction == "down"):
@@ -196,21 +206,28 @@ class Game ():
             elif (bob.direction == "right"):
                 bob.moveRight()        
                 
-            ##print (bob.direction)
-            #pygame.draw.rect(self.screen, (255,0,0) ,((bob.position),(bob.collisionWidth, bob.collisionHeight)),0)
+                
+            if self.player.time <= 0:
+                gameRunning = False
+            
+            
+            
+            
             all_sprite_list.update()
             self.screen.fill((255,255,255))
             all_sprite_list.draw(self.screen)
             
-            #print len(pygame.sprite.spritecollide(bob, bob.walls, False))
+           
             
+            self.screen.blit (self.player.timeLabel, (500,0))
             # Player Interface Draw
             cash_x = 0
             cash_y = 0
             
+           
             self.screen.blit (self.player.cashLabel, (cash_x,cash_y))
             
-            cCard_x = self.width - self.player.c_cardLabel.get_rect().width
+            cCard_x = self.width - self.player.c_cardLabel.get_rect().width - 50
             cCard_y = 0
             
             self.screen.blit (self.player.c_cardLabel,(cCard_x,cCard_y))
